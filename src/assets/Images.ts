fcd34b3fdae2aeaf379b5406ada162cf5956e591
Xiang Gu <gxgu2003@gmail.com>
2026-05-17 04:40:23 -0700

    Update Images.ts

--- src/assets/Images.ts
+++ src/assets/Images.ts
@@ -1,8 +1,8 @@
 // 1. 集中在这里引入所有图片
-import avatar from './images/My_Profile.png';
-import project1 from './images/project1.jpg';
-import project2 from './images/project2.jpg';
-// import newPic from './images/new-pic.png'; // 👈 未来新加图片，直接在这加一行
+import avatar from './Images/My_Profile.png';
+import project1 from './Images/project1.jpg';
+import project2 from './Images/project2.jpg';
+// import newPic from './Images/new-pic.png'; // 👈 未来新加图片，直接在这加一行
 
 // 2. 集中导出这张“对照表”
 export const AppImages = {
