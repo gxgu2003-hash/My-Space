a1695de2d87b1534f62e6870b35a8ea1d72a0da6
Xiang Gu <gxgu2003@gmail.com>
2026-05-22 21:40:21 -0700

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import { Linkedin, Github, Dribbble, Mail, Heart } from 'lucide-react';
+import { Linkedin, Github, Instagram, Mail, Menu, X } from 'lucide-react';
 
 export default function Footer() {
   const socialLinks = [
